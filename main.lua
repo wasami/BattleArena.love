6b428c1a5abd4963b09ef0dfa878939246a5d51d
Abdikarim Warsame <abdikarim97199@gmail.com>
2018-08-28 23:26:56 +0100

    map object
    made some progress on map object still needs some work

--- main.lua
+++ main.lua
@@ -1,9 +1,13 @@
-require 'conf'
 class = require 'Library.middleclass'
 anim8 = require 'Library.anim8'
 require 'Projectiles.projectile'
 require 'player'
 require 'bullet'
+
+
+local sti = require "sti"
+local map
+local world
 
 function love.load()
    p1 = player:new()
